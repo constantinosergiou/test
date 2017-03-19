e94fd9f55e7496c6d3341b30242d2fabd9da4e32
constantinosergiou <constantinos.s@bdswiss.com>
2017-03-19 12:10:00 +0200

    add mockapi and redux thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-//import reduxImmutableStateInvariant 'redux-immutable-state-invariant';
+//import reduxImmutableStateInvariant 'redux-immutable-state-invariant';, reduxImmutableStateInvariant()
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
-    initialState
+    initialState,
+    applyMiddleware(thunk)
   );
 }
