9f8ad2cf1d8a469115c3472fd54dbaa6aba56b72
constantinosergiou <constantinos.s@bdswiss.com>
2017-05-26 22:22:14 +0300

    error handling

--- src/actions/ajaxStatusActions.js
+++ src/actions/ajaxStatusActions.js
@@ -2,3 +2,7 @@ import * types from './actionTypes';
 export function beginAjaxCall() {
 	return {type: types.BEGIN_AJAX_CALL};
 }
+
+export function ajaxCallError() {
+	return {type: types.AJAX_CALL_ERROR};
+}
