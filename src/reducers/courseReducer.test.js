498c7cabc7d566c8650836ffd4963736397fe4e7
constantinosergiou <constantinos.s@bdswiss.com>
2017-06-24 14:18:30 +0300

    more tests

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -17,4 +17,22 @@ describe('Course Reducer', () => {
 			expect(newState[1].title).toEqual('B');
 			expect(newState[2].title).toEqual('C');
 });
+	it('should update course when passed UPDATE_COURSE_SUCCESS', () =>{
+		//arrange
+		const initialState = [
+				{id: 'A', title: 'A'},
+				{id: 'B', title: 'B'},
+				{id: 'C', title: 'C'},
+		];
+			const course = {id: 'B', title: 'New Title'};
+			const action = actions.updateCourseSuccess(course);
+			const newState = courseReducer(initialState, action);
+			const updateCourse = newState.find(a => a.id == course.id);
+			const untouchedCourse = newState.find(a=> a.id == 'A');
+
+			expect(newState.length).toEqual(3);
+			expect(updateCourse.title).toEqual('New Title');
+			expect(untouchedCoursetitle).toEqual('A');
+			
+});
 });
